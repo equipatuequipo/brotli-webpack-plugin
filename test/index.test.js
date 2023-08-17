6cd60217ba4b4b1c32feaf5a7d5f75eb0fc4b830
Alexis Santana <alexissantana@tequipo.com>
2023-08-17 16:13:57 +0200

    Updated dependencies to latest versions

--- test/index.test.js
+++ test/index.test.js
@@ -1,60 +1,65 @@
 var BrotliPlugin = require('../index.js');
 var webpack = require('webpack');
-var rmRf = require('rimraf');
+var rmRf = require('rimraf').rimrafSync;
 
 var OUTPUT_DIR = __dirname + '/tmp/';
 
+/** @param {webpack.Configuration} options */
 function createCompiler(options) {
-    var defaultOptions = {
-        bail: true,
-        cache: false,
-        entry: __dirname + '/fixtures/entry.js',
-        output: {
-            path: OUTPUT_DIR,
-            filename: '[name].js',
-            chunkFilename: '[id].[name].js'
-        },
-        plugins: [
-            new BrotliPlugin()
-        ]
-    };
+	/** @type {webpack.Configuration} */
+	var defaultOptions = {
+		bail: true,
+		cache: false,
+		stats: {
+			all: true,
+		},
+		devtool: 'eval',
+		entry: __dirname + '/fixtures/entry.js',
+		output: {
+			path: OUTPUT_DIR,
+			filename: '[name].js',
+			chunkFilename: '[id].[name].js',
+		},
+		plugins: [new BrotliPlugin()],
+	};
 
-    if (!Array.isArray(options)) {
-        options = Object.assign({}, defaultOptions, options);
-    }
+	if (!Array.isArray(options)) {
+		options = Object.assign({}, defaultOptions, options);
+	}
 
-    return webpack(options);
+	return webpack(options);
 }
 
+/** @param {webpack.Compiler} compiler */
 function compile(compiler) {
-    return new Promise(function (resolve, reject) {
-        compiler.run(function (err, stats) {
-            if (err) return reject(err);
-            resolve(stats);
-        });
-    });
+	return new Promise(function (resolve, reject) {
+		compiler.run(function (err, stats) {
+			if (err) return reject(err);
+			resolve(stats);
+		});
+	});
 }
 
 describe('when applied with default settings', function () {
-    beforeEach(function (done) {
-        rmRf(OUTPUT_DIR, done)
-    });
+	beforeEach(function () {
+		rmRf(OUTPUT_DIR);
+	});
 
-    afterEach(function (done) {
-        rmRf(OUTPUT_DIR, done)
-    });
+	afterEach(function () {
+		rmRf(OUTPUT_DIR);
+	});
 
-    it('compresses and decopresses', function () {
-        var compiler = createCompiler();
+	it('compresses and decompresses', function () {
+		var compiler = createCompiler();
 
-        new BrotliPlugin().apply(compiler);
+		new BrotliPlugin().apply(compiler);
 
-        return compile(compiler).then(function (stats) {
-            expect(stats.compilation.assets).toHaveProperty(['main.js']);
-            expect(stats.compilation.assets).toHaveProperty(['main.js.br']);
+		return compile(compiler).then(function (stats) {
+			expect(stats.compilation.assets).toHaveProperty(['main.js']);
+			expect(stats.compilation.assets).toHaveProperty(['main.js.br']);
 
-            var source = stats.compilation.assets['main.js'].source();
-            expect(source).toContain('console.log');
-        });
-    });
+			var source = stats.compilation.assets['main.js'].source();
+			expect(source).toContain('console.log');
+		});
+	});
 });
