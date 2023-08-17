8e4caf9fdd3f7a2c97db6b0705bca25173313152
Alexis Santana <alexissantana@tequipo.com>
2023-08-17 18:30:57 +0200

    Rewritten package to work with modern Webpack versions

--- compress.js
+++ compress.js
@@ -1,20 +1,29 @@
-function adapter() {
-    try {
-        var zlib = require('zlib');
-        if (zlib.hasOwnProperty('brotliCompress')) {
-            return zlib.brotliCompress;
-        }
-    } catch (err) {}
+// @ts-check
 
-    try {
-        var brotli = require('brotli');
-        return function (content, options, callback) {
-            var result = brotli.compress(content, options);
-            callback(null, result);
-        }
-    } catch (err) {
-        throw new Error('brotli not found. See https://github.com/mynameiswhm/brotli-webpack-plugin for details.');
-    }
+const zlib = require('zlib');
+
+/**
+ *
+ * @returns {(buf: Buffer, options: import('./index').BrotliLibsOptions, callback: zlib.CompressCallback) => void}
+ */
+function adapter() {
+	if (zlib.hasOwnProperty('brotliCompress')) {
+		return zlib.brotliCompress;
+	}
+
+	try {
+		const brotli = require('brotli');
+
+		return function (content, options, callback) {
+			const result = brotli.compress(content, options);
+
+			callback(null, Buffer.from(result));
+		};
+	} catch (err) {
+		throw new Error(
+			'brotli not found. See https://github.com/mynameiswhm/brotli-webpack-plugin for details.'
+		);
+	}
 }
 
-module.exports = adapter;
+module.exports = adapter();
--- index.js
+++ index.js
@@ -1,89 +1,110 @@
-var async = require('async');
-var url = require('url');
+// @ts-check
+const brotliCompress = require('./compress');
+const parsePath = require('path').parse;
+const async = require('async');
 
-var RawSource = require('webpack-sources/lib/RawSource');
+/**
+ * @typedef {import('zlib').BrotliOptions & Parameters<import('brotli')['compress']>['1']} BrotliLibsOptions
+ */
 
-function BrotliPlugin(options) {
-    options = options || {};
-    this.asset = options.asset || '[path].br[query]';
-    this.test = options.test || options.regExp;
-    this.threshold = options.threshold || 0;
-    this.minRatio = options.minRatio || 0.8;
-    this.deleteOriginalAssets = options.deleteOriginalAssets || false;
+/**
+ * @typedef {{
+ *	asset: string,
+ * 	threshold: number,
+ *  minRatio: number,
+ *  deleteOriginalAssets: boolean,
+ *  test?: RegExp
+ * } & BrotliLibsOptions} BrotliPluginOptions
+ */
 
-    var compress = require('./compress.js')();
+class BrotliPlugin {
+	/** @type {BrotliPluginOptions} */
+	static defaultOptions = {
+		asset: '[base].br',
+		threshold: 0,
+		minRatio: 0.8,
+		deleteOriginalAssets: false,
 
-    var brotliOptions = {
-        mode: options.hasOwnProperty('mode') ? options.mode : 0,
-        quality: options.hasOwnProperty('quality') ? options.quality : 11,
-        lgwin: options.hasOwnProperty('lgwin') ? options.lgwin : 22,
-        lgblock: options.hasOwnProperty('lgblock') ? options.lgblock : 0,
-        enable_dictionary: options.hasOwnProperty('enable_dictionary') ? options.enable_dictionary : true,
-        enable_transforms: options.hasOwnProperty('enable_transforms') ? options.enable_transforms : false,
-        greedy_block_split: options.hasOwnProperty('greedy_block_split') ? options.greedy_block_split : false,
-        enable_context_modeling: options.hasOwnProperty('enable_context_modeling') ? options.enable_context_modeling : false
-    };
+		// Brotli default options
+		mode: 0,
+		quality: 11,
+		lgwin: 22,
+	};
 
-    this.compress = function (content, callback) {
-        compress(content, brotliOptions, callback);
-    };
+	/**
+	 * @param {Partial<BrotliPluginOptions>} options
+	 */
+	constructor(options = {}) {
+		this.options = { ...BrotliPlugin.defaultOptions, ...options };
+	}
+
+	/**
+	 *
+	 * @param {import('webpack').Compiler} compiler
+	 */
+	apply(compiler) {
+		const pluginName = BrotliPlugin.name;
+
+		// webpack module instance can be accessed from the compiler object,
+		// this ensures that correct version of the module is used
+		// (do not require/import the webpack or any symbols from it directly).
+		const { webpack } = compiler;
+
+		// Compilation object gives us reference to some useful constants.
+		const { Compilation } = webpack;
+
+		// RawSource is one of the "sources" classes that should be used
+		// to represent asset sources in compilation.
+		const { RawSource } = webpack.sources;
+
+		// Tapping to the "thisCompilation" hook in order to further tap
+		// to the compilation process on an earlier stage.
+		compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
+			// Tapping to the assets processing pipeline on a specific stage.
+			compilation.hooks.processAssets.tap(
+				{
+					name: pluginName,
+
+					// Using one of the later asset processing stages to ensure
+					// that all assets were already added to the compilation by other plugins.
+					stage: Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_TRANSFER,
+				},
+				(assets) => {
+					async.forEach(Object.keys(assets), (filename, errorCallback) => {
+						if (this.options.test && !this.options.test.test(filename))
+							return errorCallback();
+
+						const asset = compilation.getAsset(filename);
+						if (!asset) return errorCallback();
+
+						let content = asset.source.buffer();
+						if (!Buffer.isBuffer(content)) return errorCallback();
+
+						if (content.length < this.options.threshold) return errorCallback();
+
+						brotliCompress(content, this.options, (err, result) => {
+							if (err) return errorCallback(err);
+
+							if (result.length / content.length > this.options.minRatio)
+								return errorCallback();
+
+							const parsedFilename = parsePath(filename);
+							const compressedFileName = this.options.asset.replace(
+								/\[(base|name|ext)]/g,
+								(_, part) => parsedFilename[part]
+							);
+
+							if (this.options.deleteOriginalAssets)
+								compilation.deleteAsset(filename);
+
+							compilation.emitAsset(compressedFileName, new RawSource(result));
+							errorCallback();
+						});
+					});
+				}
+			);
+		});
+	}
 }
+
 module.exports = BrotliPlugin;
-
-BrotliPlugin.prototype.apply = function (compiler) {
-    var emit = function (compilation, callback) {
-        var assets = compilation.assets;
-
-        async.forEach(Object.keys(assets), function (file, callback) {
-            if (this.test && !this.test.test(file)) {
-                return callback();
-            }
-
-            var asset = assets[file];
-            var content = asset.source();
-            if (!Buffer.isBuffer(content)) {
-                content = Buffer.from(content, 'utf-8');
-            }
-
-            var originalSize = content.length;
-            if (originalSize < this.threshold) {
-                return callback();
-            }
-
-            this.compress(content, function (err, result) {
-                if (err) {
-                    return callback(err);
-                }
-
-                if (result.length / originalSize > this.minRatio) {
-                    return callback();
-                }
-
-                var parse = url.parse(file);
-                var sub = {
-                    file: file,
-                    fileWithoutExt: file.split('.').slice(0, -1).join('.'),
-                    ext: file.split('.').slice(-1).join(''),
-                    path: parse.pathname,
-                    query: parse.query || ''
-                };
-
-                var newFile = this.asset.replace(/\[(file|fileWithoutExt|ext|path|query)]/g, function (p0, p1) {
-                    return sub[p1];
-                });
-                if (this.deleteOriginalAssets) {
-                    delete assets[file];
-                }
-                assets[newFile] = new RawSource(result);
-                callback();
-            }.bind(this));
-        }.bind(this), callback);
-    }.bind(this);
-
-    if (compiler.hooks) {
-        const plugin = { name: 'BrotliPlugin' };
-        compiler.hooks.emit.tapAsync(plugin, emit);
-    } else {
-        compiler.plugin('emit', emit);
-    }
-};
